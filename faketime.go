289e328ce7905d41336b0e09aa5b7bcd9daad3f5
haiyang <haiyang@im30.lan>
2025-01-16 12:34:12 +0800

    lock os

--- faketime.go
+++ faketime.go
@@ -6,30 +6,24 @@ package gofaketime
 import "C"
 
 import (
+	"runtime"
 	"sync"
 	"time"
 
 	"bou.ke/monkey"
 )
 
-/*
-通过猴子补丁替换time.Now()的方式来支持faketime;
-*/
-
 var lockerNow = sync.Mutex{}
 
-func getTimeFromC() int64 {
-	ch := make(chan int64, 1)
-	go func() {
-		var t C.time_t
-		ch <- int64(C.time(&t)) // 将时间存储在 t 中
-	}()
-	return <-ch
-}
 func fakeTime() time.Time {
 	lockerNow.Lock()
 	defer lockerNow.Unlock()
-	return time.Unix(getTimeFromC(), 0)
+	// 绑定到当前系统线程，避免切换到 g0
+	runtime.LockOSThread()
+	defer runtime.UnlockOSThread()
+
+	return time.Unix(int64(C.time(nil)), 0)
+
 }
 
 type FakeTime struct {
